125d059d65b2dfa580caa665b7951545f1b48579
Flávio Ferreira <flaviosf.dev@gmail.com>
2024-05-26 19:36:15 -0300

    aula01

---
+++ Aula01.playground/Contents.swift
@@ -0,0 +1,21 @@
+var name: String = "Flávio"
+var age: Int = 31
+let height: Double = 1.81
+var isMerried: Bool = true
+var gender: Character = "M"
+
+print("Name: \(name) | Age: \(age) | Height: \(height) | isMerried: \(isMerried)")
+
+// Conversor de temperatura
+let celsius = 35.7
+let fahrenheit = celsius * 9/5 + 32
+
+print("\(celsius) graus Celsius equivalem a \(fahrenheit) graus Fahrenheit.")
+
+// Cálculo da média
+let nota01 = 7.1
+let nota02 = 8.6
+let nota03 = 6.3
+let media = (nota01 + nota02 + nota03)/3
+
+print("Média: \(media)")
