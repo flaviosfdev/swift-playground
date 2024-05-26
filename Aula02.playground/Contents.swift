b8949121ebc09ab26c699eb7ed7246cc4a60d3c9
Flávio Ferreira <flaviosf.dev@gmail.com>
2024-05-26 20:23:12 -0300

    if, else if, else, operador ternario

---
+++ Aula02.playground/Contents.swift
@@ -0,0 +1,14 @@
+// Condicionais
+let numero1 = 5
+let numero2 = 5
+
+if (numero1 > numero2) {
+    print("\(numero1) é maior que \(numero2).")
+} else if numero2 > numero1 {
+    print("\(numero2) é maior que \(numero1).")
+} else {
+    print("\(numero1) é igual \(numero2).")
+}
+
+let isTernario = false
+isTernario ? print("ternário true") : print("ternário false")
