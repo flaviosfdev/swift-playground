95549cbac3dad61d9be49650938b7be20397051c
Flávio Ferreira <flaviosf.dev@gmail.com>
2024-05-27 09:46:23 -0300

    switch case

--- Aula02.playground/Contents.swift
+++ Aula02.playground/Contents.swift
@@ -23,7 +23,53 @@ let intervalorSemiAberto = 10..<20
 let numero = 15
 
 switch (numero) {
-case intervaloFechado: print("1")
-case intervalorSemiAberto: print("2")
+case 0...10: print("O \(numero) está entre 0 e 10")
+case 10...20: print("o \(numero) está entre 10 e 20")
 default: print("default")
+}
+
+let animal = "Cachorro"
+switch (animal) {
+case "Cachorro", "Gato": print("Animal doméstico")
+default: print("Animal selvagem")
+}
+
+switch (numero) {
+case let x where x % 2 == 0: print("\(x) é par")
+case let x where x % 2 != 0: print("\(x) é ímpar")
+default: break
+}
+
+let numeroAleatorio = Int.random(in: 0...100)
+print(numeroAleatorio)
+
+// Pontuacao do usuario
+let pergunta: String = "Qual o valor de 8 x 2?"
+let respostaCorreta: Int = 16
+let respostaDoUsuario: Int = 16
+var pontos: Int = 0
+
+if (respostaDoUsuario == respostaCorreta) {
+    pontos += 1
+} else {
+    pontos -= 1
+}
+print(pontos)
+
+// idade
+let idade = 18
+
+// idade do usuario
+switch(idade) {
+case 0..<13: print("Criança")
+case 13..<18: print("Adolescente")
+default: print("Adulto")
+}
+
+// op logicos e relacionais
+let estaComRgEmMaos: Bool = false
+if (idade >= 18 && estaComRgEmMaos) {
+    print("Pode entrar")
+} else {
+    print("Não pode entrar")
 }
